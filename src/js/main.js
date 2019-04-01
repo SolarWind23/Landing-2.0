1dac57e8171eabaa0e9a1273abf7cc0298a6d81c
SolarWind23 <fogebogeololo@gmail.com>
2019-04-01 14:08:57 +0300

    Create adaptive navigation bar

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,17 @@
 
 
 $(function(){
-    $(".icon").click(function(e){
+    $(".icon").click( function(e){
       
         this.classList.toggle("changed");
 
-        let menu = $('.collapce');
-        menu[0].classList.toggle("show");
+        let collapce = $('.collapce');
+        collapce[0].classList.toggle("show");
 
+        let links = $('.links');
+        links[0].classList.toggle("show");
 
         let menuItem = $(".links--item");
-       
-
-
         for(let i=0; i <= menuItem.length-1; i++) {
             menuItem[i].classList.toggle("respons");
         };
