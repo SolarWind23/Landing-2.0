95b1628d586421701bd657ea9e53e0b8e66d310a
SolarWind23 <fogebogeololo@gmail.com>
2019-04-01 18:02:05 +0300

    Create responsive menu and create section slideShow

--- src/js/main.js
+++ src/js/main.js
@@ -2,21 +2,33 @@
 
 
 $(function(){
+    let collapce = $('.collapce');
+    let links = $('.links');
+    let burger = $('.icon');
+
     $(".icon").click( function(e){
-      
         this.classList.toggle("changed");
-
-        let collapce = $('.collapce');
         collapce[0].classList.toggle("show");
-
-        let links = $('.links');
         links[0].classList.toggle("show");
-
-        let menuItem = $(".links--item");
-        for(let i=0; i <= menuItem.length-1; i++) {
-            menuItem[i].classList.toggle("respons");
-        };
-       
-        
     });
+    window.onresize = function(e){
+        let computedStyle = document.documentElement.clientWidth;
+        if(computedStyle >= 992) {
+            burger[0].classList.remove("changed")
+            collapce[0].classList.remove("show");
+            links[0].classList.remove("show");
+            
+        }
+    };
 });
+
+
+$(document).ready(function(){
+    $('.carousel').slick({
+        infinite: true,
+        arrows: true,
+        slidesToShow: 1,
+        nextArrow:'<button type="button" class="next"><i class="fas fa-chevron-right"></i></button>',
+        prevArrow:'<button type="button" class="prev"><i class="fas fa-chevron-left"></i></button>'
+    });
+  });
